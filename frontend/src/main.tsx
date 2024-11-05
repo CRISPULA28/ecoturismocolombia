2d8835978c32f71ed2b585f4489029618254657e
CRISPULA28 <umbarilaalba247@gmail.com>
2024-11-05 00:02:26 -0500

    actualizacion main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -15,7 +15,6 @@ createRoot(document.getElementById('root')!).render(
 import './home.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import React from "react";
 import Slider from "react-slick";
 
 
