b90cdf0bb3ec395b661859859551cff20988bc9c
CRISPULA28 <umbarilaalba247@gmail.com>
2024-11-04 23:15:22 -0500

    Agregar carrusel

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,6 +1,17 @@
 import './home.css';
+import React, { Component } from "react";
+import Slider from "react-slick";
 
 export function Home() {
+
+  const settings = {
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    speed: 500
+
   return (
     <div className="login-container">
       <h2>¡BIENVENIDOS!</h2>
@@ -10,6 +21,29 @@ export function Home() {
         existen en nuestro pais. ¡Enamorarte a traves de sus maravillosos lugares!
       </p>
     </div>
+
+<div className="slider-container">
+<Slider {...settings}>
+  <div>
+    <h3>1</h3>
+  </div>
+  <div>
+    <h3>2</h3>
+  </div>
+  <div>
+    <h3>3</h3>
+  </div>
+  <div>
+    <h3>4</h3>
+  </div>
+  <div>
+    <h3>5</h3>
+  </div>
+  <div>
+    <h3>6</h3>
+  </div>
+</Slider>
+</div>
   );
 }
 
