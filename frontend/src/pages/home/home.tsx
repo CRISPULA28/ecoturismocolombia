34a81cb391ef59d678deca2dc8bff004d009e92a
CRISPULA28 <umbarilaalba247@gmail.com>
2024-11-04 23:41:38 -0500

    Update home.tsx

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,61 +1,39 @@
 import './home.css';
-import React, { Component } from "react";
-import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import React from "react";
+import Slider from "react-slick";
+
 
 export function Home() {
-
-  const settings = {
-    className: "center",
-    centerMode: true,
+  var settings = {
+    dots: true,
     infinite: true,
-    centerPadding: "60px",
-    slidesToShow: 3,
-    speed: 500
-
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
-   /* <div className="login-container">
-      <h2>¡BIENVENIDOS!</h2>
-
-      <p>
-        Nuestro proposito es darte a conocer las diferentes experiencias que
-        existen en nuestro pais. ¡Enamorarte a traves de sus maravillosos lugares!
-      </p>
-    </div>*/
-
-
-<Slider {...settings}>
-<link
-  rel="stylesheet"
-  type="text/css"
-  charset="UTF-8"
-  href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-
-
-  <div>
-    <h3>1</h3>
-  </div>
-  <div>
-    <h3>2</h3>
-  </div>
-  <div>
-    <h3>3</h3>
-  </div>
-  <div>
-    <h3>4</h3>
-  </div>
-  <div>
-    <h3>5</h3>
-  </div>
-  <div>
-    <h3>6</h3>
-  </div>
- 
-</Slider>
-</div>
-/>
+    <Slider {...settings}>
+      <div>
+        <h3>1</h3>
+      </div>
+      <div>
+        <h3>2</h3>
+      </div>
+      <div>
+        <h3>3</h3>
+      </div>
+      <div>
+        <h3>4</h3>
+      </div>
+      <div>
+        <h3>5</h3>
+      </div>
+      <div>
+        <h3>6</h3>
+      </div>
+    </Slider>
   );
 }
-
 export default Home;
