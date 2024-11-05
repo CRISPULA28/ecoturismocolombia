891b5edb04be2c6dcff765941ea199f528e31147
CRISPULA28 <umbarilaalba247@gmail.com>
2024-11-05 01:21:59 -0500

    carrusel

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App.tsx';
+import ImageGallery from 'react-image-gallery';
+import "react-image-gallery/styles/css/image-gallery.css"
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
@@ -11,3 +13,25 @@ createRoot(document.getElementById('root')!).render(
     </BrowserRouter>
   </StrictMode>
 );
+
+function carrusel(){
+  const images = [
+    {
+      original: "https://picsum.photos/seed/picsum/1018/1000",
+      thumbnail: "https://picsum.photos/seed/picsum/1018/250/150",
+    }
+  
+  ]
+
+  return(
+    <div>
+      <ImageGallery />
+
+      
+    </div>
+  )
+}
+
+export default carrusel;
+
+
--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,60 +1,17 @@
 import './home.css';
-import Slider from "react-slick";
 
 
 export function Home() {
-  var settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
   return (
-    <div>
     <div className="login-container">
       <h2>¡BIENVENIDOS!</h2>
+
       <p>
         Nuestro proposito es darte a conocer las diferentes experiencias que
         existen en nuestro pais. ¡Enamorarte a traves de sus maravillosos lugares!
       </p>
-      <p></p><p></p><p></p><p></p><p></p><p></p><p></p>
     </div>
-    
-    <Slider {...settings}>
-    <link
-  rel="stylesheet"
-  type="text/css"
-  charSet="UTF-8"
-  href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-  />
-
-  <link
-  rel="stylesheet"
-  type="text/css"
-  href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-  />
-
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-      </Slider>
-      </div>
   );
 }
+
 export default Home;
