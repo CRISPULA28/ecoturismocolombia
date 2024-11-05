504ab8ee9e694a41e176067264e7a3b0cf46bacc
CRISPULA28 <umbarilaalba247@gmail.com>
2024-11-05 00:42:58 -0500

    Update home.tsx

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -22,6 +22,18 @@ export function Home() {
     </div>
     
     <Slider {...settings}>
+    <link
+  rel="stylesheet"
+  type="text/css"
+  charSet="UTF-8"
+  href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+  />
+
+  <link
+  rel="stylesheet"
+  type="text/css"
+  href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+  />
 
         <div>
           <h3>1</h3>
