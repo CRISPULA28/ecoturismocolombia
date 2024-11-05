5e127089804699a23d240ea4d2035d511143a023
CRISPULA28 <umbarilaalba247@gmail.com>
2024-11-04 23:24:07 -0500

    Update home.tsx

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,6 +1,8 @@
 import './home.css';
 import React, { Component } from "react";
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export function Home() {
 
