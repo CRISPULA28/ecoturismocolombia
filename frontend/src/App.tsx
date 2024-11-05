3cc491530583591e93842606f3f8068687f60909
CRISPULA28 <umbarilaalba247@gmail.com>
2024-11-05 03:33:38 -0500

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,15 @@ export function App() {
 
       const images = [
 
+        {
+          original: "https://www.viajerosenruta.com/wp-content/uploads/2021/05/bungee-turismo-de-aventura-1.jpg",
+        },
+        {
+          original: "https://reportelobby.info/wp-content/uploads/2023/01/TURISMO_DE_AVENTURA_REPORTE_LOBBY_06--1024x576.jpg",
+        },
+        {
+          original:"https://www.turiscom.org/media/turismo/images/2020/05/26/2020052612544841908.jpg",
+        },
         
       {
         original: "https://img.redbull.com/images/c_crop,x_69,y_0,h_3546,w_5319/c_fill,w_1500,h_1000/q_auto,f_auto/redbullcom/2016/10/04/1331821774506_1/jack-pimblett-parapente-en-cataluna",
@@ -22,15 +31,7 @@ export function App() {
         original: "https://www.viajerosenruta.com/wp-content/uploads/2021/05/rappel1-jalcomulco-turismo-de-aventura.jpg",
         
       },
-      {
-        original: "https://www.viajerosenruta.com/wp-content/uploads/2021/05/bungee-turismo-de-aventura-1.jpg",
-      },
-      {
-        original: "https://reportelobby.info/wp-content/uploads/2023/01/TURISMO_DE_AVENTURA_REPORTE_LOBBY_06--1024x576.jpg",
-      },
-      {
-        original:"https://www.turiscom.org/media/turismo/images/2020/05/26/2020052612544841908.jpg",
-      }
+     
 
     
     ]
