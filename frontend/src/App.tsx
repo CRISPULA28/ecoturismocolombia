644526207bf356625c999c952ae2baee5f16d918
CRISPULA28 <umbarilaalba247@gmail.com>
2024-11-12 21:41:37 -0500

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -24,19 +24,16 @@ export function App() {
           original:"https://www.turiscom.org/media/turismo/images/2020/05/26/2020052612544841908.jpg",
         },
         
-      {
-        original: "https://img.redbull.com/images/c_crop,x_69,y_0,h_3546,w_5319/c_fill,w_1500,h_1000/q_auto,f_auto/redbullcom/2016/10/04/1331821774506_1/jack-pimblett-parapente-en-cataluna",
-      },
-      {
-        original: "https://www.viajerosenruta.com/wp-content/uploads/2021/05/rappel1-jalcomulco-turismo-de-aventura.jpg",
+      
+       
+      
+      
+      
         
-      },
-      {
-        original:"https://www.google.com/search?q=imagenes+buceo+colombia&sca_esv=86ba961531420a8b&udm=2&biw=1536&bih=703&sxsrf=ADLYWILot-2JaxJcKxEEO-wSKJljKNbkpg%3A1730796049500"
-      },
-      {
-        original:"https://www.google.com/search?sca_esv=86ba961531420a8b&sxsrf=ADLYWILY36lU7Fwl5CtTQfHsEueNNKDZIQ:1730796444090&q=cartagena+imagen+cuatrimoto+aventura+colombia&uds"
-      }
+      
+      
+        
+      
 
     
     ]
